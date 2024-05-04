80ff13b9c8a325c4a493d3005c7d23223ae97942
Meenaksh Singhania <meenakshsinghania04@Meenakshs-MacBook-Pro.local>
2024-05-04 23:17:45 +0530

    Updated App.js and App.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [task, setTask] = useState("");
+  const [desc, setDesc] = useState("");
+  const [mainTask, setMainTask] = useState([])
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    setMainTask([...mainTask, { task, desc }])
+    setTask("")
+    setDesc("")
+  }
+
+  const deleteHandler = (i) => {
+    let copyTask = [...mainTask]
+    copyTask.splice(i, 1)
+    setMainTask(copyTask)
+  }
+
+  const completeHandler = (i) => {
+    let copyTask = [...mainTask]
+    copyTask.splice(i, 1)
+    setMainTask(copyTask)
+  }
+
+  let renderTask = <h2>No Tasks Available</h2>
+
+  if (mainTask.length > 0) {
+    renderTask = mainTask.map((t, i) => {
+      return (
+        <li key={i} className='list'>
+          <div className='renderList'>
+            <h5 className='task'>{t.task}</h5>
+            <h6 className='desc'>{t.desc}</h6>
+          </div>
+          <button className='delete' onClick={() => {
+            { deleteHandler(i) }
+          }}>Delete</button>
+          <button className='complete' onClick={() => {
+            { completeHandler(i) }
+          }}>Completed</button>
+        </li>
+      )
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>My Todo List</h1>
+      <form onSubmit={submitHandler}>
+        <input type='text' placeholder='Enter a Task here' value={task} onChange={(e) => {
+          setTask(e.target.value)
+        }} />
+        <input type='text' placeholder='Enter a Description here' value={desc} onChange={(e) => {
+          setDesc(e.target.value)
+        }} />
+
+        <button className='addtask'>Add Task</button>
+      </form>
+
+      <div className='main'>
+        <ul>
+          {renderTask}
+        </ul>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
